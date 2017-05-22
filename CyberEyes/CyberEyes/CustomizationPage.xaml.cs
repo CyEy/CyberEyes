67a251963b9c436b3b217384e3e1929560f1aa42
= <=>
2017-05-21 17:13:44 -0700

    updated controls for this page

--- CyberEyes/CyberEyes/CustomizationPage.xaml.cs
+++ CyberEyes/CyberEyes/CustomizationPage.xaml.cs
@@ -16,5 +16,50 @@ namespace CyberEyes
 		{
 			Navigation.PushAsync(new GamePage(), true);
 		}
+
+		void Handle_EasyTapped(object sender, System.EventArgs e)
+		{
+			easyFrame.BackgroundColor = Color.Accent;
+			var label = easyFrame.Content as Label;
+			label.Text = "● Easy";
+
+			mediumFrame.BackgroundColor = Color.Default;
+			label = mediumFrame.Content as Label;
+			label.Text = "○ Medium";
+
+			hardFrame.BackgroundColor = Color.Default;
+			label = hardFrame.Content as Label;
+			label.Text = "○ Hard";
+		}
+
+		void Handle_MediumTapped(object sender, System.EventArgs e)
+		{
+			easyFrame.BackgroundColor = Color.Default;
+			var label = easyFrame.Content as Label;
+			label.Text = "○ Easy";
+
+			mediumFrame.BackgroundColor = Color.Accent;
+			label = mediumFrame.Content as Label;
+			label.Text = "● Medium";
+
+			hardFrame.BackgroundColor = Color.Default;
+			label = hardFrame.Content as Label;
+			label.Text = "○ Hard";
+		}
+
+		void Handle_HardTapped(object sender, System.EventArgs e)
+		{
+			easyFrame.BackgroundColor = Color.Default;
+			var label = easyFrame.Content as Label;
+			label.Text = "○ Easy";
+
+			mediumFrame.BackgroundColor = Color.Default;
+			label = mediumFrame.Content as Label;
+			label.Text = "○ Medium";
+
+			hardFrame.BackgroundColor = Color.Accent;
+			label = hardFrame.Content as Label;
+			label.Text = "● Hard";
+		}
 	}
 }
